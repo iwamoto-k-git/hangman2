8a5c046c0aa62ba7cae06bb9d035effe5e818693
unknown <you@example.com>
2020-05-04 11:04:54 +0900

    test

---
+++ 20200427test.py
@@ -0,0 +1,36 @@
+print("one!")
+print("two!")
+print("three!")
+
+
+x = 9
+
+if x < 10:
+    print("10未満")
+else:
+    print("10以上")
+    
+y = 125
+
+if y <= 10:
+    print("10以下")
+elif 10 < y <= 25:
+    print("10より大きく25以下")
+else:
+    print("25より大きい")
+    
+
+z = 10
+
+print( z % 3)
+
+print(z // 3)
+
+age = 37
+
+if 29 < age <40:
+    print("30代")
+    
+
+
+
